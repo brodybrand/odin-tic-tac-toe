7e9ed6544328ff21a33da81fe3733819ae4562b1
Brody Brand <80013712+brodybrand@users.noreply.github.com>
2024-05-04 21:36:52 -0700

    feat: add basic UI

--- scripts.js
+++ scripts.js
@@ -5,6 +5,13 @@ function GameBoard() {
                 Cell(), Cell(), Cell()
             ];
 
+    for (i=0; i<board.length; i++) {
+        board[i].space = i;
+    }
+    const getSpaceValue = (cell) => {
+        return board[cell].getSpace();
+    }
+
     const getBoard = () => board;
 
     const takenCell = (cell) => {
@@ -39,10 +46,11 @@ function GameBoard() {
     }
 
 
-    return {getBoard, placeMarker, printBoard, takenCell, clearBoard}
+    return {getBoard, placeMarker, printBoard, takenCell, clearBoard, getSpaceValue}
 }
 
 function Cell() {
+    
     let value = 0;
 
     const addMarker = (player) => {
@@ -51,11 +59,13 @@ function Cell() {
 
     const getValue = () => value;
 
+    const getSpace = () => space;
+
     const clearCell = () => {
         value = 0;
     }
 
-    return {addMarker, getValue, clearCell}
+    return {addMarker, getValue, clearCell, getSpace}
 }
 
 
@@ -68,11 +78,11 @@ function GameController(
     const players = [
         {
             name: playerOneName,
-            marker: 1
+            marker: 'X'
         },
         {
             name: playerTwoName,
-            marker: 2
+            marker: 'O'
         }
     ];
 
@@ -132,6 +142,7 @@ function GameController(
 
     const newGame = () => {
         board.clearBoard();
+        board.printBoard();
         console.log('Game Reset')
     }
 
@@ -141,6 +152,7 @@ function GameController(
             printNewRound();
             return;
         }
+
         console.log(`${getActivePlayer().name} places ${getActivePlayer().marker} at ${cell}`)
         board.placeMarker(cell, getActivePlayer().marker)
         
@@ -148,7 +160,50 @@ function GameController(
         switchActivePlayer();
         printNewRound();
     }
-    return {playRound, getActivePlayer, switchActivePlayer, checkGameStatus}
+    return {playRound, getActivePlayer, switchActivePlayer, checkGameStatus, newGame}
 }
 
+const displayController = () => {
+    const cells = document.querySelectorAll('.cell')
+    const idConvert = {
+                    'zero': 0, 'one': 1, 'two': 2,
+                    'three': 3, 'four': 4, 'five': 5,
+                    'six': 6, 'seven': 7, 'eight': 8
+                }
+
+    const clearDisplay = () => {
+        cells.forEach((cell) => {
+            cell.textContent = '*'
+        })
+    }
+    
+    const getCellChoice = () => {
+        cells.forEach((cell, id) => {
+            cell.addEventListener('click', () => {
+                choice = id;
+                console.log(choice);
+                cell.textContent = (game.getActivePlayer().marker);
+                game.playRound(choice);
+            })
+        })
+    }
+
+
+    let resetBtn = document.querySelector('#reset-btn');
+    resetBtn.addEventListener('click', async () => {
+        console.log('reset-btn clicked')
+        game.newGame();
+        clearDisplay();
+        getCellChoice();
+    })
+
+
+    return {getCellChoice, clearDisplay}
+}
+
+
+const board = GameBoard();
+const display = displayController();
 const game = GameController();
+
+displayController().getCellChoice();
